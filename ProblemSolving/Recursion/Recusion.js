eed4943230e16b18ea6342c628c4598445058557
Christina Jasik <cjjasik@gmail.com>
2023-11-30 16:21:03 -0500

    Recusion completed w example confirms

--- ProblemSolving/Recursion/Recusion.js
+++ ProblemSolving/Recursion/Recusion.js
@@ -1,6 +1,16 @@
-const recursion = function() {
+// LIST PARAMETERS: num
+/**
+ * @param {integer} num
+ */
 
+
+const sumNums = function(num) {
+    let sum = 0;
+    for (let i = 1; i <= num; i++){
+        sum +=i;
+    }
+    return sum;
 };
 
 // TESTING IT OUT:
-console.log(recursion());
+console.log(sumNums(3)); // equals 6
