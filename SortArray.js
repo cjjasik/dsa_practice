562d77fffbea99046d0d7a4ef33c5a3161bfb5ae
Christina Jasik <cjjasik@gmail.com>
2023-12-07 16:08:21 -0500

    two methods of answering the SortArray.js

--- SortArray.js
+++ SortArray.js
@@ -20,6 +20,62 @@ Constraints:
 -5 * 104 <= nums[i] <= 5 * 104
 
 */
+// ------------------------------------------------------------------------
+
+
+// USING BUILT IN FUNCTIONS
+
+// /**
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+
+// var sortArray = function(nums) {
+//     nums.sort(function(a, b){return a - b});
+//     return nums;
+// };
+
+// console.log(sortArray([5,1,1,2,0,0]));
 
 
 
+
+// ------------------------------------------------------------------------
+
+
+
+
+// MERGE SORT ALGORITHM
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+var sortArray = function(nums) {
+    // base condition that allows the function to exit (if there's only one thing in the array, just return it).
+    if (nums.length < 2) {
+        return nums;
+    }
+    // initiate sort: find midpoint.
+    let mid = Math.floor(nums.length/2);
+    // goes until right BEFORE midpoint. slice is not inclusive.
+    let left = nums.slice(0, mid);
+    // slice will start at defined point, and go until the end of the array by default (if not given a second one).
+    let right = nums.slice(mid);
+    return merge(sortArray(left), sortArray(right));
+};
+
+var merge = function(left, right) {
+    let sortedArray = [];
+    while(left.length && right.length) {
+        if(left[0] <= right[0]){
+            sortedArray.push(left.shift())
+        } else {
+            sortedArray.push(right.shift())
+        }
+    }
+    return [...sortedArray, ...left, ...right]
+};
+
+console.log(sortArray([5, 3, 7, 1, 7]));
