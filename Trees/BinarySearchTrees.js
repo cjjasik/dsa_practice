a6b874e8d379c87bd1f2ab541078a6c71927eb42
Christina Jasik <cjjasik@gmail.com>
2023-12-14 15:15:16 -0500

    moved files, created files for binary search tree

---
+++ Trees/BinarySearchTrees.js
@@ -0,0 +1,19 @@
+/*
+OVERVIEW OF BINARY SEARCH TREES (BST)
+- A type of binary tree in which the left child of a node has a value less than the node's value and the right child has a value greater than the node's value.
+- BST property: makes it possible to efficiently search, insert, and delete elements in the tree.
+
+- BST structure:
+    - The left subtree of a node contains only nodes with keys lesser than the node’s key.
+    - The right subtree of a node contains only nodes with keys greater than the node’s key.
+    - This means everything to the left of the root is less than the value of the root and everything to the right of the root is greater than the value of the root. Due to this performing, a binary search is very easy.
+    - The left and right subtree each must also be a binary search tree.  
+    - There must be no duplicate nodes(BST may have duplicate values with different handling approaches)
+
+
+
+*/
+
+// -------------------------------------------------------------------------------------------------------------
+
+
